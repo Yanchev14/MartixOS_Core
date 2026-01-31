afd7849815435dc59bd3d22a1ecb56e18a0d9261
Martin Yanchev <martin.yanchev14@gmail.com>
2026-01-31 12:21:13 +0200

    Delete MartixOS directory

--- MartixOS/airootfs/root/customize_airootfs.sh
+++
@@ -1,9 +0,0 @@
-/bin/bash
-
-useradd -m -G wheel,video,audio,storage -s /bin/bash live-user
-passwd -d live-user
-
-sudo systemctl enable sddm
-sudo systemctl enable NetworkManager
-
-echo "live-user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
